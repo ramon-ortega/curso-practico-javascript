24f03248a72d220f71327632ce5d63e38cffdacc
Ramon de Jesus Ortega <jesus@microbit.com>
2021-10-26 14:12:28 -0500

    Convert formulas in functions

--- figuras.js
+++ figuras.js
@@ -1,45 +1,28 @@
 // Cuadrado 
+
 console.group("Cuadrado");
 
-const ladoCuadrado = 5;
-console.log(`El lado del cuadrado mide: ${ladoCuadrado}cm`);
+function perimetroCuadrado(lado){
+    return 4 * lado;
+}
 
-const perimetroCuadrado = 4 * ladoCuadrado;
-console.log(`El perimetro del cuadrado mide: ${perimetroCuadrado}cm`);
-
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log(`El area del cuadrado mide: ${areaCuadrado}cm^2`)
+function areaCuadrado(lado){
+    return lado * lado;
+}
 
 console.groupEnd();
 
 // Triangulo
 
 console.group("Triangulo");
-
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-
-console.log(
-    "El lado 1 del triangulo mide: "
-    + ladoTriangulo1
-    + "cm, "
-    + "El lado 2 del triangulo mide: "
-    + ladoTriangulo2
-    + "cm, "
-    + "La base del triangulo mide: "
-    + baseTriangulo
-    + "cm"
-    );
     
-const alturaTriangulo = 5.5;
-console.log(`La altura del triangulo mide: ${alturaTriangulo}cm`);
+function perimetroTriangulo(lado1, lado2, base){
+    return lado1 + lado2 + base;
+}
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-console.log(`El perimetro del triangulo mide: ${perimetroTriangulo}cm`);
-
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
-console.log(`El area del triangulo mide: ${areaTriangulo}cm^2`);
+function areaTriangulo(base, altura){
+    return (base * altura) / 2;
+}
 
 console.groupEnd();
 
@@ -47,22 +30,13 @@ console.groupEnd();
 
 console.group("Rectangulo");
 
-const alturaRectangulo = 4;
-const baseRectangulo = 5;
-console.log(
-    "La altura del rectangulo mide: " 
-    + alturaRectangulo
-    + "cm, "
-    + "La base del rectangulo mide: "
-    + baseRectangulo
-    + "cm, "
-);
+function perimetroRectangulo(altura, base){
+    return 2 * (altura + base);
+}
 
-const perimetroRectangulo = 2 * (alturaRectangulo + baseRectangulo);
-console.log(`El perimetro del rectangulo mide: ${perimetroRectangulo}cm`);
-
-const areaRectangulo = alturaRectangulo * baseRectangulo;
-console.log(`El area del rectangulo mide ${areaRectangulo}cm^2`);
+function areaRectangulo(altura, base){
+    return base * altura;
+}
 
 console.groupEnd();
 
@@ -73,21 +47,17 @@ console.group("Circulo");
 const PI = Math.PI;
 console.log(`El valor de PI es: ${PI}`);
 
-const radioCirculo = 5;
-const diametroCirculo = 2 * radioCirculo;
-console.log(
-    "El radio del circulo mide: "
-    + radioCirculo
-    + "cm, "
-    + "El diametro del circulo mide: "
-    + diametroCirculo
-    + "cm, "
-);
+function diametroCirculo(radio){
+    return 2 * radio;
+}
 
-const perimetroCirculo = diametroCirculo * PI;
-console.log(`El perimetro del circulo mide: ${perimetroCirculo}cm`);
+function perimetroCirculo(radio){
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+}
 
-const areaCirculo = PI * (radioCirculo * radioCirculo);
-console.log(`El area del circulo mide: ${areaCirculo}cm^2`);
+function areaCirculo(radio){
+    return PI * (radio * radio);
+}
 
 console.groupEnd();
