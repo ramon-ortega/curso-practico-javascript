2285a9f12c03ef875a4730fed2ff6c3e22bdbacd
Ramon de Jesus Ortega <jesus@microbit.com>
2021-11-05 15:21:08 -0600

    Connect JS with HTML and work with HTML to show price and discount to users

--- descuentos.js
+++ descuentos.js
@@ -4,3 +4,16 @@ function calcularPrecioConDescuento(precio, descuento) {
 
     return precioConDescuento;
 }
+
+function onButtonToCalculateDiscount(){
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const finalPrice = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const pricePrint = document.getElementById("finalPrice");
+    pricePrint.innerText = `El precio con descuento es: $${finalPrice}`;
+}
